01a9b070a5c91b3f0d38dd7d2de809b9eee8267f
Joe Masisak <joemasisak@gmail.com>
2022-08-15 13:07:20 -0700

    Complete AA coding evaluation

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 public abstract class Organization {
 
 	private Position root;
+	private int employeeNum = 1;
 	
 	public Organization() {
 		root = createOrganization();
@@ -20,8 +21,37 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
+
+		// Search through listed titles.
+		Position position = titleFinder(this.root, title);
+
+		// If title is found, place employee under title.
+		if (position != null) {
+			Optional<Employee> newEmployee = Optional.of(new Employee(this.employeeNum++, person));
+			position.setEmployee(newEmployee);
+			return Optional.of(position);
+		}
+		// If no title is found, return empty.
 		return Optional.empty();
+	}
+
+	private Position titleFinder(Position root, String title) {
+		// If title is found
+		if (root.getTitle().equals(title)) {
+			return root;
+		} else {
+			for (Position position : root.getDirectReports()) {
+				Position PositionFound = titleFinder(position, title);
+				// if title was not found
+				if (PositionFound == null) {
+					continue;
+				} else {
+					return PositionFound;
+				}
+			}
+		}
+		// Returns null if title not found
+		return null;
 	}
 
 	@Override
